9804100d3915319d39852516ba894125a84e0221
retzero <ticce@naver.com>
2022-06-24 18:53:36 +0900

    Update test.py

--- test.py
+++ test.py
@@ -6,16 +6,19 @@ import matplotlib.font_manager as fm
 from matplotlib import matplotlib_fname, get_cachedir, rc
 
 
-font_path = './NanumGothicCoding.ttf'
+font_path = './NanumGothicCoding-Regular.ttf'
 fonts_dir = os.path.join(os.path.dirname(matplotlib_fname()), 'fonts', 'ttf')
-font_cache_dir = get_cachedir()
-shutil.rmtree(font_cache_dir)
+target_fonts_path = os.path.join(fonts_dir, font_path)
 
-shutil.copyfile(font_path, os.path.join(fonts_dir, font_path))
-fontlist = fm.findSystemFonts(fontpaths=os.getcwd(), fontext='ttf')
+# 여기는 한번만 실행 하면 됨.
+if not os.path.isfile(target_fonts_path):
+    font_cache_dir = get_cachedir()
+    shutil.rmtree(font_cache_dir)
+    shutil.copyfile(font_path, target_fonts_path)
+    fontlist = fm.findSystemFonts(fontpaths=os.getcwd(), fontext='ttf')
 
 if True:
-    font_name = fm.FontProperties(fname=font_path).get_name()
+    font_name = fm.FontProperties(fname=target_fonts_path).get_name()
     rc('font', family=font_name)
 
     plt.scatter([0, 1, 2, 3, 4, 5], [1, 2, 3, 4, 5, 6])
