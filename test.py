93956384c2fc1dc83a23aa45ea66d5d2874c882f
retzero <ticce@naver.com>
2022-06-24 19:01:48 +0900

    Update test.py

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ font_path = './NanumGothicCoding.ttf'
 fonts_dir = os.path.join(os.path.dirname(matplotlib_fname()), 'fonts', 'ttf')
 target_fonts_path = os.path.join(fonts_dir, font_path)
 
-# 여기는 한번만 실행 하면 됨.
+# 여기는 한번만 실행하면 됨.
 if not os.path.isfile(target_fonts_path):
     font_cache_dir = get_cachedir()
     shutil.rmtree(font_cache_dir)
@@ -21,9 +21,8 @@ if True:
     font_name = fm.FontProperties(fname=target_fonts_path).get_name()
     rc('font', family=font_name)
 
-    plt.scatter([0, 1, 2, 3, 4, 5], [1, 2, 3, 4, 5, 6])
-    plt.title('타이틀 입니다.')
-    plt.xlabel('X축 라벨')
-    plt.ylabel('Y축 라벨')
-    plt.grid(True)
+    time_list = [18.1, 6.3, 7.2, 5.5]
+    labels = ['08시', '09시', '10시', '11시']
+
+    plt.pie(time_list, labels=labels, startangle=120)
     plt.show()
